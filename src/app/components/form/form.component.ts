dd921c675a649cf7a6601411932459d1e3396391
developer-thomas <andepipous@gmail.com>
2024-02-24 12:25:11 -0400

    fix: fixed the submit button of the form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,26 +1,25 @@
-import { Component, EventEmitter} from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { FormsService } from 'src/app/services/forms.service';
-
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
+  constructor(
+    private formBuilder: FormBuilder,
+    private formsService: FormsService,
+    private router: Router
+  ) {}
 
-  constructor(private formBuilder: FormBuilder, private formsService: FormsService) {}
-  
-  
-  namePlaceholder:string = 'e.g. Jane Appleseed'
-  numberPlaceholder: string = 'e.g. 1234 5678 9123 0000'
+  namePlaceholder: string = 'e.g. Jane Appleseed';
+  numberPlaceholder: string = 'e.g. 1234 5678 9123 0000';
   public cardForm: FormGroup = this.formsService.cardForm;
- 
-  onSubmit():void {
-    console.log(this.cardForm)
+
+  onSubmit() {
+    this.router.navigate(['sucess']);
   }
 }
-
-  
-
